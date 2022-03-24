b878bbb0a3a858fa241794e03fab06199647b7b9
angrocki <angrocki02@gmail.com>
2022-03-24 12:53:46 -0400

    Add animal_list and sort_data

---
+++ sort_data.py
@@ -0,0 +1,26 @@
+import csv 
+import pandas as pd
+
+animals = {}
+count = {}
+with open("animal_list.csv") as csvfile:
+    animal_list = csv.reader(csvfile)
+    for row in animal_list:
+        while('' in row):
+            row.remove('')
+        count[row[0]] = 0
+        animals[row[0]] = row
+        
+
+with open("data/comments/hzkbop.txt", "r") as f:
+    comments = f.read()
+    comments = comments.lower()
+    for key in animals.keys():
+       for value in animals[key]:
+           count[key] += comments.count(value)
+print(count)
+
+
+
+        
+    
