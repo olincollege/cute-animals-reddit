a3add0d32d5867b15c9a67d83283f9a26cc40f4b
Raiyan <verdant.rs@gmail.com>
2022-03-27 21:09:25 -0400

    Added find_time_data file

--- bar_graphs.py
+++ bar_graphs.py
@@ -3,24 +3,11 @@ from itertools import count
 import pandas as pd
 import ast
 import matplotlib.pyplot as plt
+"""
+Create a list of total upvotes and total comments for each animal and plots it 
+in a bar graph.
 
-'''
-with open("animal_list.csv") as csvfile:
-        animal_count = {} 
-        animal_list = csv.reader(csvfile)
-        for row in animal_list:
-            while('' in row):
-                row.remove('')
-            animal_count[row[0]] = 0
-        sorted = pd.read_fwf('data/sorted_animals.txt')
-        for animal in animal_count:
-            for value in sorted:
-                print(sorted[value])
-                if animal in sorted[value]:
-                    animal_count[animal] = animal_count[animal] + 1
-        print(animal_count)
-'''
-
+"""
 with open('data/animals_posts.txt', 'r') as animals_id:
     animals_dict = ast.literal_eval(str(animals_id.read()))
 x_axis = []
@@ -39,6 +26,7 @@ for key in animals_dict.keys():
                 ['num_comments'])
         upvotes_total.append(total_upvotes)
         comments_total.append(total_comments)
+plt.figure()
 upvote_graph = plt.subplot()
 plt.bar(x_axis,upvotes_total, width = 0.8)
 plt.title('Upvotes per Animal')
@@ -47,12 +35,12 @@ plt.ylabel('Upvotes')
 plt.setp(upvote_graph.get_xticklabels(), rotation=70)
 plt.savefig('visualizations/upvotes.png')
 
-#This needs to be fixed
+plt.figure()
 comment_graph = plt.subplot()
 plt.bar(x_axis,comments_total, width = 0.8)
 plt.title('Comments per Animal')
 plt.xlabel('Animal')
 plt.ylabel('Number of Comments')
 
-plt.setp(upvote_graph.get_xticklabels(), rotation=70)
+plt.setp(comment_graph.get_xticklabels(), rotation=70)
 plt.savefig('visualizations/comments.png')
