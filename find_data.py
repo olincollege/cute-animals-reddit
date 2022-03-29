d2866d6255d6a7306e3765de14507227d90b8be9
Raiyan <verdant.rs@gmail.com>
2022-03-28 23:40:38 -0400

    Fixed pylint on find_data

--- find_data.py
+++ find_data.py
@@ -1,22 +1,26 @@
-import praw
-from praw.models import MoreComments
+'''
+Scrapes Reddit to get all the comments and other associated data
+    from the top 1000 posts in r/awww
+'''
 import pandas as pd
-
 from config import reddit
 
-posts = []
-aww_subreddit = reddit.subreddit('aww')
-for post in aww_subreddit.top(limit=1000):
-    posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
-    submission = reddit.submission(id = str(post.id))
-    submission.comments.replace_more(limit = 5)
-    f = open(f'data/comments/{post.id}.txt', 'w')
-    for top_level_comment in submission.comments:
-        f.write(str(top_level_comment.body) + '\n')
-    f.close()
+def get_data():
+    posts = []
+    aww_subreddit = reddit.subreddit('aww')
+    for post in aww_subreddit.top(limit=1000):
+        posts.append([post.title, post.score, post.id, post.subreddit, \
+            post.url, post.num_comments, post.selftext, post.created])
+        submission = reddit.submission(id = str(post.id))
+        submission.comments.replace_more(limit = 5)
+        with open(f'data/comments/{post.id}.txt', 'w', encoding="utf8")\
+             as post_comment:
+            for top_level_comment in submission.comments:
+                post_comment.write(str(top_level_comment.body) + '\n')
 
-posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
-posts = posts.to_string()
-f = open(f'data/general_data1.txt', 'w')
-f.write(str(posts))
-f.close()
+    posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit',\
+        'url', 'num_comments', 'body', 'created'])
+    posts = posts.to_string()
+    with open('data/general_data1.txt', 'w', encoding="utf8") as general_data:
+        general_data.write(str(posts))
+get_data()
