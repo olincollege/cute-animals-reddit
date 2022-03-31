9c9a14309d6364b3d7941fb189adce2775ebe3de
Raiyan <verdant.rs@gmail.com>
2022-03-31 01:35:11 -0400

    Refactored to have more functions

--- sort_data.py
+++ sort_data.py
@@ -35,7 +35,7 @@ def find_number_mentions(post_id, data_table):
             initial_count[row[0]] = 0
             animals[row[0]] = row
         #Read each file and make every character lowercase, then counts each
-        #time an animal is mentioned in the title and comments. 
+        #time an animal is mentioned in the title and comments.
         counts = initial_count
         with open(f'data/comments/{post_id}.txt', 'r', encoding='utf8')\
              as comments:
@@ -68,31 +68,51 @@ def find_which_animal(counts):
     for key, _ in counts.items():
         total += counts[key]
     for key, _ in counts.items():
-        #Decided that threshold is 30% of all mentions and must be mentioned 5 times.
+        #Decided that threshold is 30% of all mentions and
+        #must be mentioned 5 times.
         if counts[key]/total > .3 and counts[key] > 5:
             found_animals.append(key)
     if not found_animals:
         return ['undefined']
     return found_animals
 
-post_sorted = {}
-animals_id = {}
-data = pd.read_fwf('data/general_data1.txt')
-with open('animal_list.csv', encoding='utf8') as list_animals:
-    list_animals = csv.reader(list_animals)
-    for animal_names in list_animals:
-        while '' in animal_names :
-            animal_names .remove('')
-        animals_id[animal_names[0]] = []
+def sort_posts(animal_csv):
+    '''
+    Takes in a csv where each row is all the potential names a pet
+        can be called and sorts each post into which animal it is
+    Args:
+        animal_csv: a string that is the path for a csv file
+            where each row is all the potential names an animal
+            can be called.
+    Returns:
+        post_sorted: A dictionary where each key is an id and
+            each value is a list of all the animals associated
+            with the post.
+        animals_id: A dictionary where each key is an animal
+            and each value is a list of post ids that have that
+            animal in it.
+    '''
+    post_sorted = {}
+    animals_id = {}
+    data = pd.read_fwf('data/general_data1.txt')
+    with open(animal_csv, encoding='utf8') as list_animals:
+        list_animals = csv.reader(list_animals)
+        for animal_names in list_animals:
+            while '' in animal_names :
+                animal_names .remove('')
+            animals_id[animal_names[0]] = []
 
-for ids in data.id:
-    count = find_number_mentions(ids, data)
-    post_animal = find_which_animal(count)
-    for animal in post_animal:
-        if animal != 'undefined':
-            animals_id[animal].append(ids)
-    post_sorted[ids] = post_animal
+    for ids in data.id:
+        count = find_number_mentions(ids, data)
+        post_animal = find_which_animal(count)
+        for animal in post_animal:
+            if animal != 'undefined':
+                animals_id[animal].append(ids)
+        post_sorted[ids] = post_animal
+    return post_sorted, animals_id
+#Sorts all the posts based on our list of animal names
+all_posts_sorted, all_ids_sorted = sort_posts('animal_list.csv')
 with open('data/sorted_animals.txt', 'w', encoding='utf8') as sorted_animals:
-    sorted_animals.write(str(post_sorted))
+    sorted_animals.write(str(all_posts_sorted))
 with open('data/animals_posts.txt', 'w', encoding='utf8') as animals_posts:
-    animals_posts.write(str(animals_id))
+    animals_posts.write(str(all_ids_sorted))
