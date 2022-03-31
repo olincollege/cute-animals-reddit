29e62c94b449ebeeb6375c27ed8864f29cf522d6
Raiyan <verdant.rs@gmail.com>
2022-03-31 13:14:43 -0400

    Added tests and additional function for word_clouds.py

---
+++ test_word_clouds.py
@@ -0,0 +1,16 @@
+import ast
+from wordcloud import WordCloud
+import pytest
+from word_clouds import add_comments
+
+def test_add_comments_dog():
+    '''
+    Tests that when given a singular file with just animals the comments are added properly
+    '''
+    assert(add_comments('dog', 'data/test_animals_posts.txt') == ' dog \n pupper \n lab \n wolf \n doggo \n doggie ')
+
+def test_add_comments_multiple_files():
+    '''
+    Tests that when there a multiple files for an animal the comments are added properly
+    '''
+    assert(add_comments('cat', 'data/test_animals_posts.txt') == ' Cat\n kitty\n kittens\n dog\n rabbit\n cat\n cat\n cat\n kitty\n I am raiyan siddique')
--- word_clouds.py
+++ word_clouds.py
@@ -6,22 +6,38 @@ import ast
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+def add_comments(animal, posts_for_animal):
+    '''
+    Adds all the comments for an animal into one big string
+
+    Args:
+        animal: a string that represents an animal name
+        post_for_animal: a string that is the path to a
+                txt file that has a dictionary that has
+                all the posts sorted into which animal it is.
+    Returns:
+        text: a string that is all the comments for posts with that
+            animal in it.
+    '''
+    text = ''
+    with open(posts_for_animal, 'r', encoding='utf8') as animals_id:
+        animals_dict = ast.literal_eval(str(animals_id.read()))
+    # Adds every comment from the animal to the string.
+    for post_id in animals_dict[animal]:
+        with open(f'data/comments/{post_id}.txt', 'r', encoding='utf8')\
+             as comment:
+            text += str(comment.read())
+    return text
+
 def create_word_cloud(animal):
     '''
     Takes in an animal name and creates a word cloud image
         of the most common words in the comments for posts
         with that animal.
     Args:
-        animal: a string that is an animal name
+        animal: a string that is an animal name.
     '''
-    text = ''
-    with open('data/animals_posts.txt', 'r', encoding='utf8') as animals_id:
-        animals_dict = ast.literal_eval(str(animals_id.read()))
-    # Adds every comment from the animail to the string.
-    for post_id in animals_dict[animal]:
-        with open(f'data/comments/{post_id}.txt', 'r', encoding='utf8')\
-             as comment:
-            text += str(comment.read())
+    text = add_comments(animal, 'data/animals_posts.txt')
     #Plots the word cloud and saves it to visualizations folder.
     plt.figure( figsize=(20,10) )
     word_cloud = WordCloud(width=1600, height=800).generate(text)
